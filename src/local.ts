6d282e4543efe521670528901d7e820fc37850d5
Matteo <matteo@farmerconnect.com>
2023-09-07 16:47:54 +0200

    Update...

--- src/local.ts
+++ src/local.ts
@@ -9,41 +9,40 @@ import * as restApi from './rest'
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
-  try {
-    const owner: string = 'Franklin89'
-    const repo: string = 'cicd-automation'
-    const since_tag: string = 'latest'
-    const github_token: string = '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'
+    try {
+        const owner: string = 'Franklin89'
+        const repo: string = 'cicd-automation'
+        const since_tag: string = 'latest'
+        const github_token: string = 'xxx'
 
-    const octokit = new Octokit({
-      auth: github_token
-    })
+        const octokit = new Octokit({
+            auth: github_token
+        })
 
-    // Fetch commits from the main branch
-    const commits = await restApi.getCommitsSinceTag(
-      octokit,
-      owner,
-      repo,
-      since_tag
-    )
-    console.log(`Found ${commits.length} commits since tag ${since_tag}`)
+        // Fetch commits from the main branch
+        const commits = await restApi.getCommitsSinceTag(
+            octokit,
+            owner,
+            repo,
+            since_tag
+        )
+        console.log(`Found ${commits.length} commits since tag ${since_tag}`)
 
-    // Filter and extract PR numbers
-    const prNumbers: number[] = []
-    for (const commit of commits) {
-      const prMatch = commit.commit.message.match(/\(#(\d+)\)$/)
-      if (prMatch) {
-        prNumbers.push(parseInt(prMatch[1], 10))
-      }
+        // Filter and extract PR numbers
+        const prNumbers: number[] = []
+        for (const commit of commits) {
+            const prMatch = commit.commit.message.match(/\(#(\d+)\)$/)
+            if (prMatch) {
+                prNumbers.push(parseInt(prMatch[1], 10))
+            }
+        }
+        console.log(
+            `Found ${prNumbers.length
+            } PRs in main since tag ${since_tag}: [${prNumbers.join(', ')}]`
+        )
+    } catch (error) {
+        // Fail the workflow run if an error occurs
     }
-    console.log(
-      `Found ${
-        prNumbers.length
-      } PRs in main since tag ${since_tag}: [${prNumbers.join(', ')}]`
-    )
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-  }
 }
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
